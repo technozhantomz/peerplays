a80d25f9df1e720c1135cc9108325fb7ce8c8b94
Alfredo Garcia <oxarbitrage@gmail.com>
2019-10-27 08:54:18 -0300

    fix flag

--- libraries/chain/account_evaluator.cpp
+++ libraries/chain/account_evaluator.cpp
@@ -287,7 +287,7 @@ void_result account_update_evaluator::do_apply( const account_update_operation& 
          fc::optional< bool > flag = o.extensions.value.update_last_voting_time;
          if((o.new_options->votes != acnt->options.votes ||
              o.new_options->voting_account != acnt->options.voting_account) ||
-             flag)
+             (flag.valid() && *flag))
             aso.last_vote_time = d.head_block_time();
       } );
    }
