OneGramTech/onegram-core

Externalized serialization in protocol library

libraries/app/database_api.cpp
@@ -26,6 +26,8 @@
 #include <graphene/app/util.hpp>
 #include <graphene/chain/get_config.hpp>
 #include <graphene/chain/hardfork.hpp>
+#include <graphene/protocol/address.hpp>
+#include <graphene/protocol/pts_address.hpp>
 
 #include <fc/bloom_filter.hpp>
 #include <fc/crypto/hex.hpp>

libraries/protocol/include/graphene/protocol/fee_schedule.hpp
@@ -211,3 +211,5 @@ namespace graphene { namespace protocol {
 
 FC_REFLECT_TYPENAME( graphene::protocol::fee_parameters )
 FC_REFLECT( graphene::protocol::fee_schedule, (parameters)(scale) )
+
+GRAPHENE_EXTERNAL_SERIALIZATION( extern, graphene::protocol::fee_schedule )

libraries/protocol/include/graphene/protocol/vote.hpp
@@ -24,12 +24,7 @@
 
 #pragma once
 
-#include <cassert>
-#include <cstdint>
-#include <string>
-
-#include <fc/container/flat.hpp>
-#include <fc/reflect/reflect.hpp>
+#include <graphene/protocol/types.hpp>
 
 namespace graphene { namespace protocol {
 
@@ -146,3 +141,5 @@ FC_REFLECT_TYPENAME( fc::flat_set<graphene::protocol::vote_id_type> )
 
 FC_REFLECT_ENUM( graphene::protocol::vote_id_type::vote_type, (witness)(committee)(worker)(VOTE_TYPE_COUNT) )
 FC_REFLECT( graphene::protocol::vote_id_type, (content) )
+
+GRAPHENE_EXTERNAL_SERIALIZATION( extern, graphene::protocol::vote_id_type )

libraries/protocol/proposal.cpp
@@ -107,3 +107,10 @@ void proposal_update_operation::get_required_owner_authorities( flat_set<account
 }
 
 } } // graphene::protocol
+
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::proposal_create_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::proposal_update_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::proposal_delete_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::proposal_create_operation )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::proposal_update_operation )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::proposal_delete_operation )

libraries/protocol/witness.cpp
@@ -23,6 +23,8 @@
  */
 #include <graphene/protocol/witness.hpp>
 
+#include <fc/io/raw.hpp>
+
 namespace graphene { namespace protocol {
 
 void witness_create_operation::validate() const
@@ -39,3 +41,8 @@ void witness_update_operation::validate() const
 }
 
 } } // graphene::protocol
+
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::witness_create_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::witness_update_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::witness_create_operation )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::witness_update_operation )

libraries/protocol/worker.cpp
@@ -23,6 +23,8 @@
  */
 #include <graphene/protocol/worker.hpp>
 
+#include <fc/io/raw.hpp>
+
 namespace graphene { namespace protocol {
 
 void worker_create_operation::validate() const
@@ -36,3 +38,6 @@ void worker_create_operation::validate() const
 }
 
 } }
+
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::worker_create_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::protocol::worker_create_operation )

technozhantomz/peerplays

libraries/app/database_api.cpp
@@ -26,6 +26,8 @@
 #include <graphene/chain/get_config.hpp>
 #include <graphene/chain/tournament_object.hpp>
 #include <graphene/chain/account_object.hpp>
+#include <graphene/chain/protocol/address.hpp>
+#include <graphene/chain/pts_address.hpp>
 
 #include <fc/bloom_filter.hpp>
 #include <fc/smart_ref_impl.hpp>

libraries/chain/include/graphene/chain/protocol/fee_schedule.hpp
@@ -85,3 +85,5 @@ namespace graphene { namespace chain {
 
 FC_REFLECT_TYPENAME( graphene::chain::fee_parameters )
 FC_REFLECT( graphene::chain::fee_schedule, (parameters)(scale) )
+
+GRAPHENE_EXTERNAL_SERIALIZATION( extern, graphene::chain::fee_schedule )

libraries/chain/include/graphene/chain/protocol/vote.hpp
@@ -24,12 +24,7 @@
 
 #pragma once
 
-#include <cassert>
-#include <cstdint>
-#include <string>
-
-#include <fc/container/flat.hpp>
-#include <fc/reflect/reflect.hpp>
+#include <graphene/chain/protocol/types.hpp>
 
 namespace graphene { namespace chain {
 
@@ -150,3 +145,5 @@ FC_REFLECT_TYPENAME( fc::flat_set<graphene::chain::vote_id_type> )
 
 FC_REFLECT_ENUM( graphene::chain::vote_id_type::vote_type, (witness)(committee)(worker)(VOTE_TYPE_COUNT) )
 FC_REFLECT( graphene::chain::vote_id_type, (content) )
+
+GRAPHENE_EXTERNAL_SERIALIZATION( extern, graphene::chain::vote_id_type )

libraries/chain/protocol/proposal.cpp
@@ -107,3 +107,10 @@ void proposal_update_operation::get_required_owner_authorities( flat_set<account
 }
 
 } } // graphene::chain
+
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::proposal_create_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::proposal_update_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::proposal_delete_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::proposal_create_operation )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::proposal_update_operation )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::proposal_delete_operation )

libraries/chain/protocol/witness.cpp
@@ -22,6 +22,7 @@
  * THE SOFTWARE.
  */
 #include <graphene/chain/protocol/witness.hpp>
+#include <fc/io/raw.hpp>
 
 namespace graphene { namespace chain {
 
@@ -39,3 +40,8 @@ void witness_update_operation::validate() const
 }
 
 } } // graphene::chain
+
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::witness_create_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::witness_update_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::witness_create_operation )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::witness_update_operation )

libraries/chain/protocol/worker.cpp
@@ -22,6 +22,7 @@
  * THE SOFTWARE.
  */
 #include <graphene/chain/protocol/worker.hpp>
+#include <fc/io/raw.hpp>
 
 namespace graphene { namespace chain {
 
@@ -36,3 +37,6 @@ void worker_create_operation::validate() const
 }
 
 } }
+
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::worker_create_operation::fee_parameters_type )
+GRAPHENE_EXTERNAL_SERIALIZATION( /*not extern*/, graphene::chain::worker_create_operation )