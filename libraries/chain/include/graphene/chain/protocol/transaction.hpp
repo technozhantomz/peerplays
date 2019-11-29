OneGramTech/onegram-core

Externalize serialization of blocks, tx, ops

libraries/protocol/include/graphene/protocol/transaction.hpp
@@ -294,3 +294,8 @@ FC_REFLECT( graphene::protocol::transaction, (ref_block_num)(ref_block_prefix)(e
 FC_REFLECT_DERIVED( graphene::protocol::signed_transaction, (graphene::protocol::transaction), (signatures) )
 FC_REFLECT_DERIVED( graphene::protocol::precomputable_transaction, (graphene::protocol::signed_transaction), )
 FC_REFLECT_DERIVED( graphene::protocol::processed_transaction, (graphene::protocol::precomputable_transaction), (operation_results) )
+
+GRAPHENE_EXTERNAL_SERIALIZATION(extern, graphene::protocol::transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(extern, graphene::protocol::signed_transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(extern, graphene::protocol::precomputable_transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(extern, graphene::protocol::processed_transaction)

libraries/protocol/transaction.cpp
@@ -439,3 +439,8 @@ void signed_transaction::verify_authority(
 } FC_CAPTURE_AND_RETHROW( (*this) ) }
 
 } } // graphene::protocol
+
+GRAPHENE_EXTERNAL_SERIALIZATION(/*not extern*/, graphene::protocol::transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(/*not extern*/, graphene::protocol::signed_transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(/*not extern*/, graphene::protocol::precomputable_transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(/*not extern*/, graphene::protocol::processed_transaction)

technozhantomz/peerplays

libraries/chain/include/graphene/chain/protocol/transaction.hpp
@@ -230,3 +230,8 @@ FC_REFLECT( graphene::chain::transaction, (ref_block_num)(ref_block_prefix)(expi
 // Note: not reflecting signees field for backward compatibility; in addition, it should not be in p2p messages
 FC_REFLECT_DERIVED( graphene::chain::signed_transaction, (graphene::chain::transaction), (signatures) )
 FC_REFLECT_DERIVED( graphene::chain::processed_transaction, (graphene::chain::signed_transaction), (operation_results) )
+
+
+GRAPHENE_EXTERNAL_SERIALIZATION(extern, graphene::chain::transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(extern, graphene::chain::signed_transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(extern, graphene::chain::processed_transaction)

libraries/chain/protocol/transaction.cpp
@@ -390,3 +390,7 @@ void signed_transaction::verify_authority(
 } FC_CAPTURE_AND_RETHROW( (*this) ) }
 
 } } // graphene::chain
+
+GRAPHENE_EXTERNAL_SERIALIZATION(/*not extern*/, graphene::chain::transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(/*not extern*/, graphene::chain::signed_transaction)
+GRAPHENE_EXTERNAL_SERIALIZATION(/*not extern*/, graphene::chain::processed_transaction)