e55075ec6d5aeacfb9e69a43bead9d7d75dc159b
gladcow <s.gladkov@pbsa.info>
2020-01-17 18:51:44 +0300

    fix CLI tests

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -991,6 +991,7 @@ void application::initialize(const fc::path& data_dir, const boost::program_opti
       wanted.push_back("witness");
       wanted.push_back("account_history");
       wanted.push_back("market_history");
+      wanted.push_back("bookie");
    }
    int es_ah_conflict_counter = 0;
    for (auto& it : wanted)
