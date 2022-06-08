03e37896d5d2cd03a2ce21c5a0f833b27a40606c
Davor Hirunda <mdmdavor@gmail.com>
2022-06-08 22:02:40 +0000

    Cancel the thread for sync blocks

--- libraries/net/node.cpp
+++ libraries/net/node.cpp
@@ -888,7 +888,7 @@ namespace graphene { namespace net { namespace detail {
     void node_impl::p2p_network_connect_loop()
     {
       VERIFY_CORRECT_THREAD();
-      while (!_p2p_network_connect_loop_done.canceled())
+      while (!_p2p_network_connect_loop_done.canceled() && !_node_is_shutting_down)
       {
         try
         {
@@ -3961,6 +3961,8 @@ namespace graphene { namespace net { namespace detail {
     void node_impl::close()
     {
       VERIFY_CORRECT_THREAD();
+
+      _node_is_shutting_down = true;
 
       try
       {
